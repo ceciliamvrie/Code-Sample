313621d5d4e6376cf852a5d62014bf2384f75430
Cecilia Cisneros <cmvrie@utexas.edu>
2017-11-16 15:14:21 -0600

    Add files via upload

--- test/components/MainTest.js
+++ test/components/MainTest.js
@@ -4,8 +4,9 @@
 'use strict';
 
 // Uncomment the following lines to use the react test utilities
-// import React from 'react/addons';
-// const TestUtils = React.addons.TestUtils;
+import React from 'react/addons';
+const TestUtils = React.addons.TestUtils;
+
 import createComponent from 'helpers/shallowRenderHelper';
 
 import Main from 'components/Main';
