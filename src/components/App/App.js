eda95508e903780a59a3edea6101b25d2bb56616
Cecilia Cisneros <cmvrie@utexas.edu>
2017-11-17 09:22:47 -0600

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import CarrierCardContainer from '../../containers/CarrierCardContainer/CarrierCardContainer'
 
+// App maps through the carrier card data props and passes each card into a carrier card container component
 const App = ({ carrierCards }) => (
     <div>
       {
